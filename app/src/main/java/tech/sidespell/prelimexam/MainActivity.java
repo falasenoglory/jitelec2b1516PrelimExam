fba22e556faabf53b3e6d5cc004203f322392c50
Shanyl Jimenez <falsenoglory@gmail.com>
2015-12-15 17:31:19 +0800

    Prelim Exams

--- app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
+++ app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
@@ -1,12 +1,32 @@
 package tech.sidespell.prelimexam;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+
+    ////////////////// VARIABLES ///////////////////////////////
+    private RadioButton rdbIncrement;
+    private RadioButton rdbDecrement;
+    private TextView txtSpeed;
+    private SeekBar seekBar;
+    private TextView txtTime;
+    private ToggleButton btnOnOf;
+    private int speed;
+    private int time;
+
+    ////////////////////////////////////////////////////////////
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +35,42 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        rdbIncrement= (RadioButton)findViewById(R.id.rdbIncrement);
+        rdbDecrement= (RadioButton)findViewById(R.id.rdbDecrement);
+        txtSpeed=(TextView)findViewById(R.id.txtSpeed);
+        seekBar=(SeekBar)findViewById(R.id.seekBar);
+        txtTime=(TextView)findViewById(R.id.txtTime);
+        btnOnOf=(ToggleButton)findViewById(R.id.tglOnOff);
+
+        btnOnOf.setOnCheckedChangeListener(this);
+
     }
+
+    final Handler handler = new Handler();
+    final Runnable runnable = new Runnable() {
+
+        @Override
+        public void run() {
+
+            int speed = Integer.parseInt(txtSpeed.getText().toString());
+            int time = Integer.parseInt(txtTime.getText().toString());
+            if (rdbIncrement.isChecked()) {
+                time++;
+
+                txtTime.setText(time + "");
+                handler.postDelayed(runnable, speed);
+            } else if (rdbDecrement.isChecked()) {
+                time--;
+                txtTime.setText(time + "");
+
+            }
+
+        }
+    };
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,4 +93,24 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        speed=Integer.parseInt(txtSpeed.getText().toString());
+        if(isChecked)
+        {
+            handler.postDelayed(runnable, speed);
+        }
+
+
+
+
+
+    }
+
+
+
+
+
 }
